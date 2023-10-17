1fcd083549eab8cb8e06904dbc31571635de16ec
Samuel Kerstell <samuel.kerstell@outlook.com>
2023-10-17 23:37:49 +0000

    challenge 3or4

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import css from './App.module.css';
 import Sidebar from "./components/Sidebar";
 //import NavBarSimple from './components/NavBarSimple';
 import NavBarForm from './components/NavBarForm';
+import Content from './components/Content';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       {/* <NavBarSimple /> */}
       <NavBarForm />
       <Sidebar />
+      <Content />
     </div>
   );
 }
